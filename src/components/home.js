9da0eae8f4fd023f0c56b23db1ded1c251471e6a
Handriel137 <r.solnik@gmail.com>
2018-04-25 22:46:09 -0400

    final complete

--- src/components/home.js
+++ src/components/home.js
@@ -5,92 +5,190 @@ import leftpad from 'left-pad'
 
 const API_BASE = "http://localhost:3000/";
 
+class ActiveFormatter extends React.Component {
+
+  sendSomething(props){
+    console.log("Stupid functions are stupid");
+    console.log(props.character)
+  }
+
+  render() {
+return (
+  <button type='button' onClick={this.sendSomething}>trade {this.props.id}
+    {this.props.character_id}</button>
+);
+  }
+}
+
+function activeFormatter(cell, row) {
+  return (<ActiveFormatter item={row.id} character={row.character_id}/>);
+}
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       characters: [],
-      items: [],
+      items1: [],
+      items2: [],
       character_id: 1,
+      character_id_1: 1,
+      character_id_2: 1,
       value: 0
-      // formMode: "new",
-      // character: { name: "", job: "", level: 0, player: "" }
+      // formMode: "new", character: { name: "", job: "", level: 0, player: "" }
     };
 
-    this.loadCharacters = this.loadCharacters.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.loadItems = this.loadItems.bind(this);
+    this.loadCharacters = this
+      .loadCharacters
+      .bind(this);
+    this.handleChangeCharacterOne = this
+      .handleChangeCharacterOne
+      .bind(this);
+    this.handleChangeCharacterTwo = this
+      .handleChangeCharacterTwo
+      .bind(this);
+    this.loadItems1 = this
+      .loadItems1
+      .bind(this);
+      this.loadItems2 = this
+      .loadItems2
+      .bind(this);
 
   }
 
   loadCharacters() {
-    axios.get(`${API_BASE}/characters`).then(res => {
-      this.setState({characters: res.data});
-      console.log(`Characters loaded! = ${this.state.characters}`)
-    }).catch(err => console.log(err));
+    axios
+      .get(`${API_BASE}/characters`)
+      .then(res => {
+        this.setState({characters: res.data});
+        console.log(`Characters loaded! = ${this.state.characters}`)
+      })
+      .catch(err => console.log(err));
   }
 
-  loadItems() {
-    axios.get(`${API_BASE}/characters/${this.state.character_id}/items`).then(res => {
-      this.setState({items: res.data});
-      console.log(`Items loaded! = ${this.state.items}`)
-    }).catch(err => console.log(err));
+  loadItems1() {
+    axios
+      .get(`${API_BASE}/characters/${this.state.character_id_1}/items`)
+      .then(res => {
+        this.setState({items1: res.data});
+        console.log(`Items loaded! = 1 ${this.state.items1}`)
+      })
+      .catch(err => console.log(err));
 
-    axios.get(`${API_BASE}/characters/${this.state.character_id}`).then(res => {
-      this.setState({character: res.data});
-      console.log(`Character2 loaded! = ${this.state.items}`)
-    }).catch(err => console.log(err));
+    // axios
+    //   .get(`${API_BASE}/characters/${this.state.character_id_1}`)
+    //   .then(res => {
+    //     this.setState({character: res.data});
+    //     console.log(`Character loaded! = ${this.state.items1}`)
+    //   })
+    //   .catch(err => console.log(err));
   }
 
+loadItems2() {
+  axios
+    .get(`${API_BASE}/characters/${this.state.character_id_2}/items`)
+    .then(res => {
+      this.setState({items2: res.data});
+      console.log(`Items loaded! = ${this.state.items2}`)
+    })
+    .catch(err => console.log(err));
+
+  // axios
+  //   .get(`${API_BASE}/characters/${this.state.character_id_2}`)
+  //   .then(res => {
+  //     this.setState({character: res.data});
+  //     console.log(`Character loaded! = ${this.state.items2}`)
+  //   })
+  //   .catch(err => console.log(err));
+}
   componentDidMount() {
     console.log("Characters just got mounted")
     this.loadCharacters();
-    this.loadItems();
+    this.loadItems1();
+    this.loadItems2();
   }
 
-  handleChange(event) {
+  handleChangeCharacterOne(event) {
     console.log(this.state.character_id);
-    this.setState({character_id: event.target.value});
-    console.log(this.state.character_id);
-    this.loadItems();
+    this.setState({character_id_1: event.target.value});
+    console.log(this.state.character_id_1);
+
+    axios
+      .get(`${API_BASE}/characters/${this.state.character_id_1}/items`)
+      .then(res => {
+        this.setState({items1: res.data});
+        console.log(`Character 1's Items loaded! = ${this.state.items1}`)
+      })
+      .catch(err => console.log(err));
+
+    // this.loadItems();
     console.log(this.state.items);
 
   }
+
+  handleChangeCharacterTwo(event) {
+    this.setState(this.state);
+    console.log(this.state.character_id_2);
+this.setState({character_id_2: event.target.value});
+    console.log(this.state.character_id_2);
+
+    axios
+      .get(`${API_BASE}/characters/${this.state.character_id_2}/items`)
+      .then(res => {
+        this.setState({items2: res.data});
+        console.log(`Character 2's Items loaded! = ${this.state.items2}`)
+      })
+      .catch(err => console.log(err));
+
+    // this.loadItems();
+    console.log(this.state.items2);
+
+  }
+
+
   render() {
 
-    return (<div>
-      <div className="jumbotron">
-        <div className="container">
-          {/* <h1>Draw your Blade!</h1> */}
-          <p>This is a single page app for manageing a character's inventory for Dungeons & Dragons.</p>
+    return (
+      <div>
+        <div className="jumbotron">
+          <div className="container">
+            <h1>Draw your Blade!</h1>
+            <p>This is a single page app for manageing a character's inventory for Dungeons
+              & Dragons.</p>
 
+          </div>
         </div>
-      </div>
-      <table align="center" >
+        <table align="center">
 
           <td className="col-md-4">Character 1
             <thead>
-              <select name="Character 1" onChange={this.handleChange} value={this.state.value}>
-                {
-                  this.state.characters.map(character => {
-                    return <option value={character.character_id} key={character.id}>{character.id}</option>
+              <select
+                name="Character 1"
+                onChange={this.handleChangeCharacterOne}
+                value={this.state.character_id_1}>
+                {this
+                  .state
+                  .characters
+                  .map(character => {
+                    return <option value={character.id} key={character.id}>{character.name}</option>
                   })
-                }
+}
               </select>
               <hr></hr>
 
               <tbody>
 
-                <BootstrapTable data={this.state.items} striped hover bordered = {false}>
+                <BootstrapTable data={this.state.items1} striped hover bordered={false}>
                   <TableHeaderColumn dataField='name' width='200' dataSort isKey="isKey">Item Name</TableHeaderColumn>
-                  <TableHeaderColumn dataField='description'width='300'>Item Description</TableHeaderColumn>
+                  <TableHeaderColumn dataField='description' width='300'>Item Description</TableHeaderColumn>
                   <TableHeaderColumn dataField='rarity' width='100'>Item Rarity</TableHeaderColumn>
+                  <TableHeaderColumn dataFormat = {activeFormatter}> Buttons </TableHeaderColumn>
                 </BootstrapTable>
 
               </tbody>
 
-              <p>{this.state.character_id}</p>
+              <p>{this.state.character_id_1}</p>
 
             </thead>
 
@@ -100,33 +198,40 @@ class Home extends React.Component {
           </td> */}
           <td className="col-md-4">Character 2
             <thead>
-              <select name="Character 2" onChange={this.handleChange} value={this.state.value}>
-                {
-                  this.state.characters.map(character => {
-                    return <option value={character.character_id} key={character.id}>{character.id}</option>
+              <select
+                name="Character 2"
+                onChange={this.handleChangeCharacterTwo}
+                value={this.state.character_id_2}>
+                {this
+                  .state
+                  .characters
+                  .map(character => {
+                    return <option value={character.id} key={character.id}>{character.name}</option>
                   })
-                }
+}
               </select>
               <hr></hr>
 
               <tbody>
 
-                <BootstrapTable data={this.state.items} striped hover bordered = {false}>
+                <BootstrapTable data={this.state.items2} striped hover bordered={false}>
                   <TableHeaderColumn dataField='name' width='200' dataSort isKey="isKey">Item Name</TableHeaderColumn>
-                  <TableHeaderColumn dataField='description'width='300'>Item Description</TableHeaderColumn>
+                  <TableHeaderColumn dataField='description' width='300'>Item Description</TableHeaderColumn>
                   <TableHeaderColumn dataField='rarity' width='100'>Item Rarity</TableHeaderColumn>
+                  <TableHeaderColumn dataFormat = {activeFormatter}> Buttons </TableHeaderColumn>
                 </BootstrapTable>
 
               </tbody>
 
-              <p>{this.state.character_id}</p>
+              <p>{this.state.character_id_2}</p>
 
             </thead>
 
           </td>
 
-      </table>
-    </div>);
+        </table>
+      </div>
+    );
   }
 }
 
